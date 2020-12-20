0a8986be617ae3fb4ecc60e2a77d5d0c0a860c3e
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2020-12-20 23:08:56 +0100

    Upload files

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  // data.push({id: 12, name: 'ade', age: 25, image: 'ade'})
+
+  const [people, setPeople] = useState(data)
+
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} Birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  )
 }
 
 export default App;
